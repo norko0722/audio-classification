9a0a9ca98997185c14b4bc542df2b0977f413a3e
Norbert <norbert.balucha@kosickaakademia.sk>
2025-11-18 21:47:23 +0100

    add imports in header part of main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse
+import librosa
+import numpy as np
+from collections import Counter
+from services.model_service import model_service
+import tempfile
+import os
 
 app = FastAPI()
 
