ac76b1dc28c8b0dde183f426b599978e2f04cebc
Norbert <norbert.balucha@kosickaakademia.sk>
2025-11-19 17:39:23 +0100

    add wav fiel validation and tmp storage fo /classification endpoint

--- backend/main.py
+++ backend/main.py
@@ -13,32 +13,24 @@ app = FastAPI()
 async def root():
     return { "message": "Audio Genre Classification Project Norbert Balucha - FastAPI" }
 
-@app.pos("/classification")
+@app.post("/classification")
 async def audio_classification(file: UploadFile = File(...), segment_duration: int = 10):
     if not file.filename:
         raise HTTPException(
             status_code=400,
             detail="No file uploaded!"
         )
-    else:
-        if not file.endswith(".wav"):
-            raise HTTPException(
-                status_code=400,
-                detail="File must be in .wav format"
-            )
-        else:
-            audio, sr = librosa.load(file_path, sr=22050)
-            total_duration = librosa.get_duration(y=audio, sr=sr)
-            segment_samples = int(segment_duration * sr)
+    if not file.endswith(".wav"):
+        raise HTTPException(
+            status_code=415,
+            detail="File must be in .wav format"
+        )
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
+            tmp.write(await file.read())
+            tmp_path = tmp.name
 
-            predicted_genres = []
-
-            for i in range(0, len(audio), segment_samples):
-                segment = audio[i : i + segment_samples]
-
-                if len(segment) < segment_samples:
-                    print(f"Segment length {len(segment)} is less than {segment_samples}, skipping segment.")
-                continue
-            
-                genre = model_service.predict(segment, sr)
-                predicted_genres.append(genre)
+    except Exception:
+        raise HTTPException(status_code=500, detail='Something went wrong')
+    finally:
+        file.file.close()
