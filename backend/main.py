f6797dafef772efcb843d0850033bd4defa7c370
Norbert <norbert.balucha@kosickaakademia.sk>
2025-11-19 17:18:36 +0100

    add simple validation for uplodaed file in /classification endpoint

--- backend/main.py
+++ backend/main.py
@@ -13,3 +13,32 @@ app = FastAPI()
 async def root():
     return { "message": "Audio Genre Classification Project Norbert Balucha - FastAPI" }
 
+@app.pos("/classification")
+async def audio_classification(file: UploadFile = File(...), segment_duration: int = 10):
+    if not file.filename:
+        raise HTTPException(
+            status_code=400,
+            detail="No file uploaded!"
+        )
+    else:
+        if not file.endswith(".wav"):
+            raise HTTPException(
+                status_code=400,
+                detail="File must be in .wav format"
+            )
+        else:
+            audio, sr = librosa.load(file_path, sr=22050)
+            total_duration = librosa.get_duration(y=audio, sr=sr)
+            segment_samples = int(segment_duration * sr)
+
+            predicted_genres = []
+
+            for i in range(0, len(audio), segment_samples):
+                segment = audio[i : i + segment_samples]
+
+                if len(segment) < segment_samples:
+                    print(f"Segment length {len(segment)} is less than {segment_samples}, skipping segment.")
+                continue
+            
+                genre = model_service.predict(segment, sr)
+                predicted_genres.append(genre)
