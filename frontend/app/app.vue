84d295f8be858fba636f29ecb938e66a169b443b
norko0722 <mrraptor219@gmail.com>
2025-12-08 17:48:24 +0100

    add frontend files with structure

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <Header />
+  <AudioUpload />
 </template>
+
---
+++ frontend/app/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="bg-gray-800 text-white shadow">
+    <div class="px-4 py-3 flex items-center justify-center gap-3">
+        <div>
+          <div class="font-semibold text-lg text-center">Audio Classification</div>
+          <div class="text-sm text-gray-300 hidden sm:block">Upload audio and get predictions</div>
+        </div>
+    </div>
+  </header>
+</template>
+
