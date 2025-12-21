29134bb45e239346832682d297b2ac65b73bd777
norko0722 <mrraptor219@gmail.com>
2025-12-21 21:19:46 +0100

    create AboutUs.vue component with html structure and styles

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <Header />
-  <AudioUpload />
+  <div class="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
+    <Header />
+    <AboutUs />
+  </div>
 </template>
 
