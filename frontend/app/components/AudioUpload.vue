9965f6ac74245a47cb535af32b2ffc491f9e9d8e
norko0722 <mrraptor219@gmail.com>
2025-12-21 21:03:01 +0100

    edit header by new design and html structure

--- frontend/app/components/AudioUpload.vue
+++ frontend/app/components/AudioUpload.vue
@@ -1,8 +1,5 @@
 <template>
-    <div class="flex flex-col justify-center items-center mt-10 gap-4">
-        <label class="items-center" for="file_input">Upload file</label>
-        <input class="items-center" type="file" id="file_input" accept="audio/*" />
-    </div>
+
 </template>
 
 
