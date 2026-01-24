9d40c61fb1929b4677ade502705edbbf76d54588
norko0722 <mrraptor219@gmail.com>
2026-01-24 19:26:34 +0100

    add /history/if endpoint GET and database insert with records

--- backend/database/database.py
+++ backend/database/database.py
@@ -6,7 +6,7 @@
 """
 
 import os
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, func
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
