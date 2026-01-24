1844a8fc772af74da2cf7a4879e42590dbe1bbb6
norko0722 <mrraptor219@gmail.com>
2026-01-24 19:19:27 +0100

    add model for classification audio file in models.py

--- backend/database/database.py
+++ backend/database/database.py
@@ -6,7 +6,7 @@
 """
 
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -14,7 +14,6 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./users.db")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
+Base = declarative_base() 
 
 Base.metadata.create_all(bind=engine)
