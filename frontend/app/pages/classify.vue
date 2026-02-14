3851a191c317f47808fa9c82d398b45a6f1503d1
norko0722 <mrraptor219@gmail.com>
2026-02-14 19:38:23 +0100

    add in /classify redirect, signout and username in header

--- frontend/app/pages/classify.vue
+++ frontend/app/pages/classify.vue
@@ -32,7 +32,7 @@
                 </nav>
 
                 <div class="flex items-center gap-4">
-                    <span class="text-green-400 font-medium">balucha.norbert</span>
+                    <span class="text-green-400 font-medium">{{ userName }}</span>
                     <button @click="signOut" class="flex items-center gap-2 px-4 py-2 bg-gradient-to-r from-green-600 to-emerald-600 text-white rounded-lg hover:from-green-700 hover:to-emerald-700 transition-all">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out">
                             <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
@@ -49,6 +49,37 @@
     </div>
 </template>
 
-<script setup>
-    await fetchHistory()
+<script setup lang="ts">
+    import { useRouter, useRoute } from 'vue-router'
+    import Upload from '@/components/Upload.vue'
+    const userName = ref('')
+
+    const userString = localStorage.getItem('user')
+
+    if (userString) {
+        const user = JSON.parse(userString)
+        userName.value = user.name
+        console.log(userName)
+    }
+
+    const router = useRouter()
+    const route = useRoute()
+
+    function goToClassify() {
+        if (route.path !== '/classify') {
+            router.push('/classify')
+        }
+    }
+
+    function goToHistory() {
+        if (route.path !== '/history') {
+            router.push('/history')
+        }
+    }
+
+    function signOut() {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    router.push('/sign-in')
+    }
 </script>
