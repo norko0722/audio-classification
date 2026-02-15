e7198974da1d8bf82ff983dcb31b42e4be895b23
norko0722 <mrraptor219@gmail.com>
2026-02-15 18:56:23 +0100

    fix name for username in BE

--- backend/database/models.py
+++ backend/database/models.py
@@ -7,8 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, nullable=True)
-    surname = Column(String, index=True, nullable=True)
+    username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
